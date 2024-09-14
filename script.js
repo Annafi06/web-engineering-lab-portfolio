cbbacebf323f419c1101959e927bace80ebf5e60
Annafi06 <abdunnafi28@gmail.com>
2024-09-15 01:34:34 +0600

    shesh

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-// Toggle dark mode and light mode
 const themeIcon = document.getElementById('theme-icon');
 
 themeIcon.addEventListener('click', function () {
     document.body.classList.toggle('light-mode');
 
-    // Apply light mode styles to navbar and other elements
     document.querySelector('.navbar').classList.toggle('light-mode');
     document.querySelectorAll('nav ul li a').forEach(link => {
         link.classList.toggle('light-mode');
@@ -12,10 +10,9 @@ themeIcon.addEventListener('click', function () {
     document.querySelector('.logo').classList.toggle('light-mode');
     document.querySelector('.hire-me').classList.toggle('light-mode');
 
-    // Change icon based on mode
     if (document.body.classList.contains('light-mode')) {
-        themeIcon.src = 'moon.png';  // Change to moon icon for dark mode
+        themeIcon.src = 'moon.png';
     } else {
-        themeIcon.src = 'sun.png';  // Change to sun icon for light mode
+        themeIcon.src = 'sun.png';
     }
 });
